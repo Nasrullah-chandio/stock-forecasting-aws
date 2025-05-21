a2634ebc5d20a05de6abb7d94c0bdca392ec35e6
Nasrullah-chandio <nchandio.pk@gmail.com>
2025-05-21 00:28:52 -0400

    Initial VPC setup using Terraform

---
+++ provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  cloud {
+    organization = "stock-forecast-aws"
+
+    workspaces {
+      name = "stock-forecast-cli"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
